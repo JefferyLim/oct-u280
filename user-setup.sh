683eb851c7d74c41d44429b8e52b35b48ae5f092
JefferyLim <jjlim@bu.edu>
2025-06-06 12:41:29 -0400

    Fixing user script

--- user-setup.sh
+++ user-setup.sh
@@ -2,9 +2,9 @@
 
 install_custom_lib(){
     sudo apt-get install -y iperf
-    git clone https://github.com/JefferyLim/corundum 
-    git clone https://github.com/JefferyLim/OS4C
-    git clone https://github.com/JefferyLim/oct_helper
+    git clone https://github.com/JefferyLim/corundum ~/corundum 
+    git clone https://github.com/JefferyLim/OS4C ~/OS4C
+    git clone https://github.com/JefferyLim/oct_helper ~/oct_helper
 }
 
 install_custom_lib
