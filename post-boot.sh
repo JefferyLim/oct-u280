f323875540a9872babe7524d64832407843af51c
JefferyLim <jjlim@bu.edu>
2025-06-07 13:27:23 -0400

    Last fix...

--- post-boot.sh
+++ post-boot.sh
@@ -230,7 +230,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 if [ $USER != $GENIUSER ]; then
-    sudo -u $GENIUSER $SCRIPTPATH/user-setup.sh
+    sudo -u $GENIUSER $SCRIPT_PATH/user-setup.sh
     exit $?
 fi
 
