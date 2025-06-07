fc697aac787f94f9e38ce297b7789e16bb0665de
JefferyLim <jjlim@bu.edu>
2025-06-07 13:51:06 -0400

    Testing path

--- user-setup.sh
+++ user-setup.sh
@@ -4,18 +4,18 @@ echo "Running user script"
 
 install_custom_lib(){
     sudo apt-get install -y iperf
-    git clone https://github.com/JefferyLim/corundum ~/corundum 
-    git clone https://github.com/JefferyLim/OS4C ~/OS4C
-    git clone https://github.com/JefferyLim/oct_helper ~/oct_helper
+    git clone https://github.com/JefferyLim/corundum /user/jlim/corundum 
+    git clone https://github.com/JefferyLim/OS4C /user/jlim/OS4C
+    git clone https://github.com/JefferyLim/oct_helper /user/jlim/oct_helper
 }
 
 install_custom_lib
 
 
-cd ~/corundum/modules/mqnic
+cd /user/jlim/corundum/modules/mqnic
 make
 cd ../../utils/
 make
 
-cd ~/OS4C/modules/mqnic
+cd /user/jlim/OS4C/modules/mqnic
 make
