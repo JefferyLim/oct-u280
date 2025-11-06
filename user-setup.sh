34af6182e4ed2af4e647f98f4940c3596a77d478
JefferyLim <jjlim@bu.edu>
2025-11-06 04:02:32 -0500

    Adding alias functions

--- user-setup.sh
+++ user-setup.sh
@@ -4,8 +4,8 @@ echo "Running user script"
 
 install_custom_lib(){
     sudo apt-get install -y iperf git
-    git clone https://github.com/JefferyLim/corundum /users/jlim/corundum 
-    git clone https://github.com/JefferyLim/OS4C /users/jlim/OS4C
+    git clone -b jlim/dev https://github.com/JefferyLim/corundum /users/jlim/corundum 
+    git clone -b vswitch/dev https://github.com/JefferyLim/OS4C /users/jlim/OS4C
     git clone https://github.com/JefferyLim/oct_helper /users/jlim/oct_helper
 }
 
@@ -18,3 +18,17 @@ make
 
 cd /users/jlim/OS4C/modules/mqnic
 make
+
+# File to modify
+BASHRC="$HOME/.bashrc"
+
+# Lines to add
+cat << 'EOF' >> "$BASHRC"
+alias thesisdriver="cd ~/OS4C/modules/mqnic"
+alias resetsetup="sudo virsh destroy myvm; sudo virsh create ~/projects/OCT/oct_helper/vm/vm.xml; sudo virsh console myvm"
+alias scripts="cd ~/OS4C/scripts/new"
+export PATH="$PATH:/user/jlim/OS4C/fpga/lib/pcie/scripts:/user/jlim/oct-helper/fpga/:/user/jlim/oct-helper/vm/:/user/jlim/oct-helper/network"
+EOF
+
+
+
