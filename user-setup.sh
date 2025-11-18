b09cc1cea0e85adf7b115ae617b2f96efaae7c16
JefferyLim <jjlim@bu.edu>
2025-11-17 22:41:50 -0500

    Fixing script

--- user-setup.sh
+++ user-setup.sh
@@ -6,6 +6,7 @@ install_custom_lib(){
     sudo apt-get install -y iperf git
     git clone -b jlim/dev https://github.com/JefferyLim/corundum /users/jlim/corundum 
     git clone -b vswitch/dev https://github.com/JefferyLim/OS4C /users/jlim/OS4C
+    git clone https://github.com/JefferyLim/oct_helper.git
     wget https://github.com/esnet/iperf/releases/download/3.16/iperf-3.16.tar.gz
     tar -xvf iperf-3.16.tar.gz
     cd iperf-3.16/
