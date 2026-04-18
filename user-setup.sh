94678b59fab3f7f523defce3c6b32dd827b3adef
Jeffery Lim <17893528+JefferyLim@users.noreply.github.com>
2026-04-17 21:51:02 -0400

    Enhance user setup script with sentinel check
    Added a sentinel file check to prevent re-running the script. Updated the PATH export and cleaned up unnecessary commands.

--- user-setup.sh
+++ user-setup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
-
 echo "Running user script"
+
+SENTINEL="/users/$USER/.user_script_done"
+if [ -f "$SENTINEL" ]; then
+    echo "User script already ran; exiting."
+    exit 0
+fi
 
 install_custom_lib(){
     sudo apt-get remove iperf3 
@@ -19,27 +24,20 @@ install_custom_lib(){
 }
 
 cd /users/jlim/
-
 mkdir -p /users/jlim/vm-results
-
-
 install_custom_lib
-
 cd /users/jlim/corundum/modules/mqnic
 make
 cd ../../utils/
 make
-
 # File to modify
 BASHRC="$HOME/.bashrc"
-
 # Lines to add
 cat << 'EOF' >> "$BASHRC"
 alias thesisdriver="cd ~/OS4C/modules/mqnic"
 alias resetsetup="sudo virsh destroy myvm; sudo virsh create ~/projects/OCT/oct_helper/vm/vm.xml; sudo virsh console myvm"
 alias scripts="cd ~/OS4C/scripts/new"
-export PATH="$PATH:/user/jlim/OS4C/fpga/lib/pcie/scripts:/user/jlim/oct-helper/fpga/:/user/jlim/oct-helper/vm/:/user/jlim/oct-helper/network"
+export PATH="$PATH:/user/jlim/OS4C/fpga/lib/pcie/scripts:/user/jlim/oct-helper/fpga/:/user/jlim/oct-helper/network"
 EOF
 
-
-
+touch "$SENTINEL"
